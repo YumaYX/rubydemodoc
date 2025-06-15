e07014bb4b561de5fadfc71fd856c22e4d5058c4
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-06-15 23:56:25 +0900

    update

--- lib/makeindex.rb
+++ lib/makeindex.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 require 'fileutils'
+require_relative 'app'
 
 def generate_index(rb_files, input_dir: 'input', output_dir: 'output', index_file: 'output/index.md', link_prefix: '')
   FileUtils.mkdir_p(File.dirname(index_file))
 
   entries = {}
-  rb_files.sort.each do |rb_path|
-    relative_path = rb_path.sub(%r{\A#{Regexp.escape(input_dir)}/}, '')
-    folder = relative_path.split('/').first
-    md_relative = relative_path.sub(/\.rb\z/, '.md')
 
-    entries[folder] ||= []
-    entries[folder] << { md_relative: md_relative, rb_name: File.basename(rb_path) }
+  rb_files.sort.each do |rb_path|
+    parser = FilePathParser.new(rb_path)
+    md_filename = "#{parser.parent_name}_#{parser.base_name.sub(/\.rb$/, '.md')}"
+    html_filename = md_filename.sub(/\.md$/, '.html')
+
+    entries[parser.parent_name] ||= []
+    entries[parser.parent_name] << {
+      link: File.join(link_prefix, html_filename).gsub('\\', '/'),
+      display: parser.base_name
+    }
   end
 
   File.open(index_file, 'w') do |f|
@@ -21,13 +26,10 @@ def generate_index(rb_files, input_dir: 'input', output_dir: 'output', index_fil
     entries.each do |folder, files|
       f.puts "## #{folder}\n\n"
       files.each do |file|
-        file_name_html = "#{File.basename(file[:md_relative], '.md')}.html"
-        link_path = File.join(link_prefix, file_name_html).gsub('\\', '/')
-        display_name = file[:rb_name]
-
-        f.puts "- [#{display_name}](#{link_path})"
+        f.puts "- [#{file[:display]}](#{file[:link]})"
       end
       f.puts "\n"
     end
   end
 end
+
