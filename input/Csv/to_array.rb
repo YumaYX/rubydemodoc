2ca2f800a8a590d2a1db1ccffa298c937288cecd
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-06-17 00:00:40 +0900

    udpate

--- input/Csv/to_array.rb
+++
@@ -1,5 +0,0 @@
-require 'ys1'
-
-csv_file_name = "input/Csv/to_hash.csv"
-
-YS1::Csv.to_array(csv_file_name)
--- input/FileHasher/run_by_name.rb
+++
@@ -1,5 +0,0 @@
-require 'ys1'
-
-hasher = YS1::FileHasher.new(["lib/**/*.rb"])
-
-hasher.run_by_name
