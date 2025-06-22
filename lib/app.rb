ae96404328b0efe1250d45f63851210f6233ddee
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-06-22 20:13:21 +0900

    app.rb の更新

--- lib/app.rb
+++ lib/app.rb
@@ -92,6 +92,7 @@ def process_file(path)
     if File.exist?(md_path)
       f.puts File.read(md_path)
     else
+      f.puts "\n**Heads up:** This description was created by AI and might not be 100% accurate.\n"
       description = describe_codes(path)
       File.write(md_path, description)
       f.puts description
