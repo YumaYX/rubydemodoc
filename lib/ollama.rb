f4c62242f09fdb20c6cbf105ac4cffcd8affe7a3
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-07-19 19:49:18 +0900

    Update ollama.rb

--- lib/ollama.rb
+++ lib/ollama.rb
@@ -7,7 +7,7 @@ require 'json'
 def ollama_request(prompt)
   url = URI.parse('http://localhost:11434/api/generate')
   data = {
-    "model": 'gemma3n',
+    "model": 'gemma3',
     "prompt": prompt,
     "stream": false
   }
